3577472a6b2aa31898697e283353225cd81a776d
Problem889 <xiongziheng25@163.com>
2026-01-31 09:44:17 +0800

    Add files via upload

---
+++ 向量表示/vector_in_movement.py
@@ -0,0 +1,21 @@
+from playLA.Vector import Vector
+
+if __name__ == "__main__":
+    vec1 = Vector([4,2])
+    print(vec1)
+u = Vector([7,3])
+print(u)
+a = Vector([5,5])
+print(a)
+b = Vector([6,9])
+print(b)
+
+print("vec1[0]={},vec1[1]={}".format(vec1[0],vec1[1]))
+print("u[0]={},u[1]={}".format(u[0],u[1]))
+print("a[0]={},a[1]={}".format(a[0],a[1]))
+print("b[0]={},b[1]={}".format(b[0],b[1]))
+
+print(len(vec1))
+print(len(u))
+print(len(a))
+print(len(b))
